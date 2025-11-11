03b652e337d641e0da21a3b694fe946b3ce1c341
YaMoef <brentclaessens@outlook.com>
2025-11-11 16:44:10 +0100

    feat(musicTrack)!: add new migration for size and duration props
    BREAKING CHANGE: a new ef core migration has been added

---
+++ backend/Stross.Infrastructure/Migrations/20251110191411_AddSizeAndDurationToMusicTrack.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stross.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSizeAndDurationToMusicTrack : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "MusicTracks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Size",
+                table: "MusicTracks",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "MusicTracks");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "MusicTracks");
+        }
+    }
+}
